7948b8dbb535a17161511a441181505955fb43b4
I-Chanwoo <chanuel0110@gmail.com>
2025-01-05 14:27:53 +0900

    feat: Add new home URL front page

---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { FaCartShopping } from "react-icons/fa6"; // Cart icon
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const [headerColor, setHeaderColor] = useState("#fbfbfb"); // 헤더 초기 색상
+  const [color, setColor] = useState("#010101"); // 글 색 초기 색상
+  const [boxShadow, setBoxShadow] = useState("none"); // 초기 색상
+
+
+
+  // 스크롤 위치에 따라 색상 변경
+  useEffect(() => {
+    const handleScroll = () => {
+
+      if (window.scrollY > 70) {
+        setHeaderColor("rgba(1, 1, 1, 0.7)"); // 스크롤이 50px 이상일 때 어두운 색상
+        setColor("#fbfbfb")
+        setBoxShadow("0px 5px 10px rgba(0, 0, 0, 0.2)")
+
+      } else {
+        setHeaderColor("#fbfbfb"); // 기본 색상
+        setColor("#010101")
+        setBoxShadow("none")
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: headerColor, // 스크롤에 따라 배경색 변경
+        color:color,
+        boxShadow: boxShadow,
+        backdropFilter: "blur(10px)",
+        transition: "background-color 0.3s ease-out", // 색상 변경에 부드러운 전환 효과
+        paddingLeft: "2rem",
+        paddingRight: "2rem"
+
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1, marginTop: "0px" }} >
+          <IconButton color="inherit"  style={{ padding: "10px", margin: "0px" }} onClick={() => {navigate("/home")}} >
+            <FaCartShopping size="1.2rem" />
+          </IconButton>
+        </Typography>
+        <Button color="inherit">
+          모의 면접
+        </Button>
+        <Button color="inherit">
+          내 히스토리
+        </Button>
+        &nbsp;&nbsp;&nbsp;
+        <Button color="inherit" variant="outlined">
+          로그인
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
